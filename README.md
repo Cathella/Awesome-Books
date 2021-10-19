d67f9269df008139b16e66573cd8ac43d9267033
vicmaburrito <vmaldaraca@gmail.com>
2021-10-19 13:03:14 -0500

    Fix linter errors

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ![](https://img.shields.io/badge/Microverse-blueviolet)
 
-# Project Name
+# Awesome Books
 
 > Basic website that allows users to add/remove books from a list.
 
--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 // book class and constructor
-class Book { 
-  constructor(id, title, author) { 
-    this.id = id; 
-    this.title = title; 
+class Book {
+  constructor(id, title, author) {
+    this.id = id;
+    this.title = title;
     this.author = author;
-  } 
+  }
 }
 
 // get books from localstorage
-const getBooks = () => { 
+const getBooks = () => {
   let books;
 
-  if (localStorage.getItem('books') === null) { 
-    books = []; 
-	} else { 
-    books = JSON.parse(localStorage.getItem('books')); 
-  } 
-  
-	return books; 
-}
+  if (localStorage.getItem('books') === null) {
+    books = [];
+  } else {
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+
+  return books;
+};
 
 // function to add a new book to the collection, with title and author.
 const addBook = (book) => {
   const books = getBooks();
   books.push(book);
   localStorage.setItem('books', JSON.stringify(books));
-}
+};
 
 // add submit book to browser view
 const addBookToList = (book) => {
-  const list = document.getElementById("bookList");
+  const list = document.getElementById('bookList');
 
-  const row = document.createElement("div");
+  const row = document.createElement('div');
   row.innerHTML = `
     <div>
     <div>${book.title}</div>
@@ -43,53 +43,53 @@ const addBookToList = (book) => {
   `;
 
   list.appendChild(row);
-}
+};
 
 // display books
 const displayBooks = () => {
   const books = getBooks();
   books.forEach((book) => addBookToList(book));
-}
+};
 
 // remove book from browser view
 const deleteBook = (el) => {
   if (el.classList.contains('delete'));
   el.parentElement.remove();
-}
+};
 
 // delete book from local storage
 const removeBook = (id) => {
   const books = getBooks();
 
   books.forEach((book, index) => {
-    if(book.id === id) {
+    if (book.id === id) {
       books.splice(index, 1);
     }
   });
 
   localStorage.setItem('books', JSON.stringify(books));
-}
+};
 
 // display books list on window load
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', () => {
   displayBooks();
-})
+});
 
 // Add book Event
-document.getElementById("form").addEventListener('submit', (e) => {
+document.getElementById('form').addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
   const id = `${title}${author}`;
 
   const book = new Book(id, title, author);
   addBookToList(book);
   addBook(book);
-})
+});
 
 // To remove a book event
-document.getElementById("bookList").addEventListener("click",  (e) => {
+document.getElementById('bookList').addEventListener('click', (e) => {
   deleteBook(e.target);
   removeBook(e.target.previousElementSibling.textContent);
 });
