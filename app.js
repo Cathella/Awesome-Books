d2b849002ed2c0fac1e159ceceb9beb4deed140d
vicmaburrito <vmaldaraca@gmail.com>
2021-10-19 00:26:16 -0500

    Add getBooks

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ class Book {
     this.author = author;
   } 
 }
+
+const getBooks = () => { 
+    let books; 
+    if (localStorage.getItem('books') === null) { 
+        books = []; } 
+        else { 
+            books = JSON.parse(localStorage.getItem('books')); 
+    } 
+    return books; 
+} 
