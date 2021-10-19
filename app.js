c025e6ee5a874af1c597a8ba6b4da4fcbf5f5bf3
Cathella <ncathie214@gmail.com>
2021-10-19 17:41:40 +0300

    add addBook, addBookToList methods

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 class Book { 
-    constructor(id, title, author) { 
+  constructor(id, title, author) { 
     this.id = id; 
     this.title = title; 
     this.author = author;
@@ -7,11 +7,37 @@ class Book {
 }
 
 const getBooks = () => { 
-    let books; 
-    if (localStorage.getItem('books') === null) { 
-        books = []; } 
-        else { 
-            books = JSON.parse(localStorage.getItem('books')); 
-    } 
-    return books; 
-} 
+  let books;
+
+  if (localStorage.getItem('books') === null) { 
+    books = []; 
+	} else { 
+    books = JSON.parse(localStorage.getItem('books')); 
+  } 
+  
+	return books; 
+}
+
+// function to add a new book to the collection, with title and author.
+const addBook = (book) => {
+  const books = getBooks();
+  books.push(book);
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+const addBookToList = (book) => {
+  const list = document.getElementById("bookList");
+
+  const row = document.createElement("div");
+  row.innerHTML = `
+    <div>
+    <div>${book.title}</div>
+    <div>${book.author}</div>
+    <div class="hidden">${book.id}</div>
+    <button class="delete">Remove</button>
+    <hr>
+    </div>
+  `;
+
+  list.appendChild(row);
+}
