3ef0816b8b504a401d077571a8af4bcdd62ac4c6
Cathella <ncathie214@gmail.com>
2021-10-19 17:58:27 +0300

    add book methods and events

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// book class and constructor
 class Book { 
   constructor(id, title, author) { 
     this.id = id; 
@@ -6,6 +7,7 @@ class Book {
   } 
 }
 
+// get books from localstorage
 const getBooks = () => { 
   let books;
 
@@ -25,6 +27,7 @@ const addBook = (book) => {
   localStorage.setItem('books', JSON.stringify(books));
 }
 
+// add submit book to browser view
 const addBookToList = (book) => {
   const list = document.getElementById("bookList");
 
@@ -41,3 +44,52 @@ const addBookToList = (book) => {
 
   list.appendChild(row);
 }
+
+// display books
+const displayBooks = () => {
+  const books = getBooks();
+  books.forEach((book) => addBookToList(book));
+}
+
+// remove book from browser view
+const deleteBook = (el) => {
+  if (el.classList.contains('delete'));
+  el.parentElement.remove();
+}
+
+// delete book from local storage
+const removeBook = (id) => {
+  const books = getBooks();
+
+  books.forEach((book, index) => {
+    if(book.id === id) {
+      books.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+// display books list on window load
+window.addEventListener('DOMContentLoaded', function() {
+  displayBooks();
+})
+
+// Add book Event
+document.getElementById("form").addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const id = `${title}${author}`;
+
+  const book = new Book(id, title, author);
+  addBookToList(book);
+  addBook(book);
+})
+
+// To remove a book event
+document.getElementById("bookList").addEventListener("click",  (e) => {
+  deleteBook(e.target);
+  removeBook(e.target.previousElementSibling.textContent);
+});
